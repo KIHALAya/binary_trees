c8963b77f4dad6ca4b8b78a2fc2f3d08e59928e1
KIHALAya <kihalaya00@.com>
2024-01-04 00:35:48 -0800

    Task 3 - function that deletes an entire binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /* Basic Binary Tree */
 /**
@@ -37,6 +38,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
 
